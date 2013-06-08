9c04333aaa21c888e38a0c4093cb2974316aca3b
Plankton <Plankton@172.20.17.129>
2013-06-08 13:50:09 +0200

    paletka added

--- src/GameObiects/Paletka.java
+++ src/GameObiects/Paletka.java
@@ -1,12 +1,27 @@
 package GameObiects;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+
 
 public class Paletka extends GameObiect{
-
-    @Override
-    public void paintMe() {
-	
+    int side;
+    public Paletka(Image image, Point position) {
+	super(image, position);
     }
+
+ 
+    
+    public void moveme(int value){
+	
+      if(side==0){
+	  this.position.translate(0,value);	  
+      }
+      else 
+	  this.position.translate(value, 0);
+    }
+
     
     
 
